bda53b6f7ad0242470ec37461d1ad2e3a668762f
ixior462 <pawel.krupski@mail.com>
2019-10-03 18:00:35 +0100

    initial commit

---
+++ src/main/java/edu/mwo/registry/ApplicationInitializer.java
@@ -0,0 +1,13 @@
+package edu.mwo.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInitializer.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mwo/registry/ApplicationInitializerTests.java
@@ -0,0 +1,16 @@
+package edu.mwo.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationInitializerTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
