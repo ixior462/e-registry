bcde26e759f026e8be805ac7e94343509b518f09
kycz <czaplakaro@gmail.com>
2019-11-08 10:21:48 +0100

    Display a list of existing teachers and students

--- src/eRegistryFrontend/src/app/components/user-view/user-view.component.ts
+++ src/eRegistryFrontend/src/app/components/user-view/user-view.component.ts
@@ -9,6 +9,14 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class UserViewComponent implements OnInit {
 
+  // todo: fetch list of existing students from DB
+  students = ['Student1', 'Student2', 'Student3', 'Student11',
+  'Student22', 'Student33', 'Student111', 'Student222', 'Student333',
+  'Student1111', 'Student2212', 'Student3313'];
+
+  // todo: fetch list of existing teachers from DB
+  teachers = ['Teacher1', 'Teacher2', 'Teacher3', 'Teacher11', 'Teacher12', 'Teacher13', 'Teacher21', 'Teacher22', 'Teacher23'];
+
   constructor(
       private router: Router,
       private authService: AuthService,
