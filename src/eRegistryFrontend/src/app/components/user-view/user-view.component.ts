5c7178c45869cf15a78032138eba72ce6a4af073
kycz <czaplakaro@gmail.com>
2019-11-09 14:25:30 +0100

    New buttons for adding / deleting entities

--- src/eRegistryFrontend/src/app/components/user-view/user-view.component.ts
+++ src/eRegistryFrontend/src/app/components/user-view/user-view.component.ts
@@ -12,7 +12,7 @@ export class UserViewComponent implements OnInit {
   // todo: fetch list of existing students from DB
   students = ['Student1', 'Student2', 'Student3', 'Student11',
   'Student22', 'Student33', 'Student111', 'Student222', 'Student333333333',
-  'Student1111', 'Student2212', 'Student3313'];
+  'Student1111', 'Student2212', 'Student3313', 'Student11111', 'Student22112', 'Student33113'];
 
   // todo: fetch list of existing teachers from DB
   teachers = ['Teacher1', 'Teacher2', 'Teacher3', 'Teacher11', 'Teacher12', 'Teacher13', 'Teacher21', 'Teacher22', 'Teacher23'];
@@ -23,6 +23,22 @@ export class UserViewComponent implements OnInit {
     ) { }
 
   ngOnInit() {
+  }
+
+  // todo: for each of these: open alert window with options (name / surname)
+  public addTeacher() {
+  }
+
+  public deleteTeacher() {
+
+  }
+
+  public addStudent() {
+
+  }
+
+  public deleteStudent() {
+
   }
 
   public logOut() {
