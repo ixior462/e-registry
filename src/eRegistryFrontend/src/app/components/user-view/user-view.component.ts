4ba3180cf7de3d651a7d459abe31e060ab5b394c
kycz <czaplakaro@gmail.com>
2019-11-13 12:25:06 +0100

    Pop up windows for adding and deleting entities

--- src/eRegistryFrontend/src/app/components/user-view/user-view.component.ts
+++ src/eRegistryFrontend/src/app/components/user-view/user-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-user-view',
